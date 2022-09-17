0fe251a1d9c4358608a6b7ccc4e92df1dfe01a61
Rieul <38723811+rieulp@users.noreply.github.com>
2022-09-18 01:32:32 +0900

    Time: 255 ms (29.68%), Space: 48.9 MB (30.48%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -6,8 +6,12 @@ var lengthOfLongestSubstring = function(s) {
     const n = new Set(s).size;
     for(let i = n; i > 0; i--) {
         for(let j = 0; j <= s.length-i; j++) {
-            const str = s.substring(j, j + i);
-            if(str.length === new Set(str).size) return str.length;
+            const set = new Set();
+            for(let k=j; k<j+i; k++) {
+                if(set.has(s[k])) break;
+                set.add(s[k]);
+            }
+            if(set.size===i) return i;
         }
     }
     return 0;
