bd06f7d6fe6e746d2c28916741f470e64d719d78
Rieul <38723811+rieulp@users.noreply.github.com>
2022-11-21 19:21:31 +0900

    Time: 91 ms (93.54%), Space: 51 MB (46.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,8 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for(let i=0; i<nums.length;i++) {
-        if(i!==nums.lastIndexOf(nums[i])) return true;
-    }
-    return false;
+    return new Set(nums).size!==nums.length
 };
