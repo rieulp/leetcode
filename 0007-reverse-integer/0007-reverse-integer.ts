d941850f67a4510f091d4b3b486f0a565a92ecf7
Rieul <38723811+rieulp@users.noreply.github.com>
2024-07-05 23:07:47 +0900

    Time: 66 ms (94.89%), Space: 53.6 MB (65.15%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.ts
+++ 0007-reverse-integer/0007-reverse-integer.ts
@@ -1,8 +1,7 @@
 function reverse(x: number): number {
-    const isPositive = x >= 0;
-    const MAX = Math.pow(2,31);
-    const reversedNum = +([...(isPositive ? x : Math.abs(x)).toString()].reverse().join(''));
-    if(isPositive && reversedNum>MAX-1) return 0;
-    if(!isPositive && reversedNum>MAX) return 0;
-    return reversedNum * (isPositive ? 1 : -1);
-};
+  const MAX = Math.pow(2, 31);
+  const isPositive = x >= 0;
+  const reversedNum = +[...(isPositive ? x : Math.abs(x)).toString()].reverse().join("");
+  if (isPositive ? reversedNum > MAX - 1 : reversedNum > MAX) return 0;
+  return reversedNum * (isPositive ? 1 : -1);
+}
