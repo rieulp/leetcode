52698c596f721f3d72473f145c7d4d6eb6df2032
Rieul <38723811+rieulp@users.noreply.github.com>
2024-07-05 22:49:15 +0900

    Time: 102 ms (39.93%), Space: 57.9 MB (40.34%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.ts
+++ 0006-zigzag-conversion/0006-zigzag-conversion.ts
@@ -1,12 +1,11 @@
 function convert(s: string, numRows: number): string {
-    const board:string[][] = Array.from({length:numRows},()=>[]);
-    let flag = 1;
-    for(let i=0, row=0; i<s.length; i++){
-        board[row%numRows].push(s[i]);
-        if(row===0) flag = 1;
-        else if(row === numRows-1) flag = -1;
-        row+= flag;
-    }
-    return board.map(item=>item.join('')).join('');
-    
-};
+  const board: string[][] = Array.from({ length: numRows }, () => []);
+  let flag = 1;
+  for (let i = 0, row = 0; i < s.length; i++) {
+    board[row % numRows].push(s[i]);
+    if (row === 0) flag = 1;
+    else if (row === numRows - 1) flag = -1;
+    row += flag;
+  }
+  return board.map((item) => item.join("")).join("");
+}
