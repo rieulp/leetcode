7e8cd0a35c2b9325476f33384a7278702318b4d4
RaYeoung <38723811+rieulp@users.noreply.github.com>
2024-12-24 18:45:19 +0900

    Time: 16 ms (6.6%), Space: 56.1 MB (5.94%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -1,14 +1,11 @@
 function isValid(s: string): boolean {
-    const stack = [];
-    const left = ['(', '{', '['];
-    const right = [')', '}', ']']
+    const regexp = /(\(\)|\{\}|\[\])/g;
 
-    const brackets = left.reduce((acc, cur, i) => ({ ...acc, [right[i]]: cur }), {});
-
-    for (let i = 0; i < s.length; i++) {
-        if (left.includes(s[i])) stack.push(s[i]);
-        else if (stack.length && stack[stack.length - 1] === brackets[s[i]]) stack.pop();
-        else return false;
+    while (s.length > 0) {
+        let result = s.replace(regexp, '');
+        if (result === s) return false;
+        s = result;
     }
-    return stack.length === 0
+    
+    return true;
 };
