971f437ff0b28abf573e3a646a17cd0252d25afa
RaYeoung <38723811+rieulp@users.noreply.github.com>
2025-01-10 00:10:56 +0900

    Time: 14 ms (90.43%), Space: 62 MB (85.64%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.ts
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.ts
@@ -2,23 +2,15 @@ function largestRectangleArea(heights: number[]): number {
     const stack = []
     let largestRect = 0;
 
-    heights.push(0)
+    for (let i = 0; i <= heights.length; i++) {
+        const h = i === heights.length ? 0 : heights[i];
 
-    for (let i = 0; i < heights.length; i++) {
-        const h = heights[i];
-        let lastIndex = -1;
         while (stack.length && heights[stack[stack.length - 1]] > h) {
             const index = stack.pop()
             const curHeight = heights[index]
-            const size = (i - index) * curHeight;
-
+            const width = i - (stack.length > 0 ? stack[stack.length - 1] : -1) - 1;
+            const size = width * curHeight;
             largestRect = Math.max(largestRect, size)
-            lastIndex = index;
-        }
-
-        if(lastIndex >= 0) {
-            stack.push(lastIndex)
-            heights[lastIndex] = h
         }
 
         stack.push(i)
